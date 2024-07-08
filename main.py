bf7468d36df767c73861f32ec69d04ef21c21891
İbrahim Yılmaz <ibrahimenes020@gmail.com>
2024-07-08 14:20:52 +0300

    Added titles of the projects

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import streamlit as st
-from PIL import Image
-
-image = Image.open("images/MyPhoto.png")
+import pandas as pd
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image(image, use_column_width=True)
+    st.image("images/MyPhoto.png", use_column_width=True)
 
 with col2:
     st.header("Ibrahim Enes Yılmaz")
@@ -16,3 +14,19 @@ with col2:
             """
     st.info(content)
 
+st.write("Here you can find the projects i did so far.")
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df.iterrows():
+        if index % 2 == 0:
+            st.header(row["title"])
+            st.image()
+
+with col4:
+    for index, row in df.iterrows():
+        if index % 2 == 1:
+            st.header(row["title"])
